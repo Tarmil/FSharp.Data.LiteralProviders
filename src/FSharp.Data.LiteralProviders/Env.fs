c96331c97902436bdb3358792a759f75816818b0
Loic Denuziere <ldenuziere@d-edge.com>
2019-08-16 16:22:50 +0200

    Implement EnvOrDefault

--- src/FSharp.Data.LiteralProviders/Env.fs
+++ src/FSharp.Data.LiteralProviders/Env.fs
@@ -12,9 +12,8 @@ type EnvProvider(config) as this =
     let ns = "FSharp.Data.LiteralProviders"
     let asm = Assembly.GetExecutingAssembly()
 
-    let createTypes() =
+    let createEnv() =
         let ty = ProvidedTypeDefinition(asm, ns, "Env", None)
-
         for entry in Environment.GetEnvironmentVariables() do
             let entry = entry :?> System.Collections.DictionaryEntry
             let name = entry.Key :?> string
@@ -22,8 +21,26 @@ type EnvProvider(config) as this =
             ProvidedField.Literal("Name", typeof<string>, name) |> thisTy.AddMember
             ProvidedField.Literal("Value", typeof<string>, entry.Value) |> thisTy.AddMember
             thisTy |> ty.AddMember
+        ty
 
-        [ty]
+    let createEnvOrDefault() =
+        let ty = ProvidedTypeDefinition(asm, ns, "EnvOrDefault", None)
+        ty.DefineStaticParameters(
+            [ProvidedStaticParameter("Name", typeof<string>); ProvidedStaticParameter("DefaultValue", typeof<string>, "")],
+            fun tyName args ->
+                let ty = ProvidedTypeDefinition(asm, ns, tyName, None)
+                let name = args.[0] :?> string
+                let envValue = Environment.GetEnvironmentVariable(name)
+                let isSet = not (isNull envValue)
+                let value = if isSet then envValue else args.[1] :?> string
+                ProvidedField.Literal("Name", typeof<string>, name) |> ty.AddMember
+                ProvidedField.Literal("Value", typeof<string>, value) |> ty.AddMember
+                ProvidedField.Literal("IsSet", typeof<bool>, isSet) |> ty.AddMember
+                ty)
+        ty
+
+    let createTypes() =
+        [createEnv(); createEnvOrDefault()]
 
     do this.AddNamespace(ns, createTypes())
 
--- tests/FSharp.Data.LiteralProviders.Tests/Env.fs
+++ tests/FSharp.Data.LiteralProviders.Tests/Env.fs
@@ -8,3 +8,43 @@ module Env =
     [<Test>]
     let ``OS is either Windows_NT or Unix`` () =
         Assert.Contains(Env.OS.Value, [|"Windows_NT"; "Unix"|])
+
+module EnvOrDefault =
+
+    type ``OS without default`` = EnvOrDefault<"OS">
+    type ``OS with default`` = EnvOrDefault<"OS", "Invalid">
+
+    [<Test>]
+    let ``OS without default is set`` () =
+        Assert.IsTrue(``OS without default``.IsSet)
+
+    [<Test>]
+    let ``OS with default is set`` () =
+        Assert.IsTrue(``OS with default``.IsSet)
+
+    [<Test>]
+    let ``OS without default is either Windows_NT or Unix`` () =
+        Assert.Contains(``OS without default``.Value, [|"Windows_NT"; "Unix"|])
+
+    [<Test>]
+    let ``OS with default is either Windows_NT or Unix`` () =
+        Assert.Contains(``OS with default``.Value, [|"Windows_NT"; "Unix"|])
+
+    type ``Garbage without default`` = EnvOrDefault<"SomeGarbageVariableThatShouldntBeSet">
+    type ``Garbage with default`` = EnvOrDefault<"SomeGarbageVariableThatShouldntBeSet", "some default value">
+
+    [<Test>]
+    let ``Garbage variable without default is not set`` () =
+        Assert.IsFalse(``Garbage without default``.IsSet)
+
+    [<Test>]
+    let ``Garbage variable without default is empty string`` () =
+        Assert.AreEqual("", ``Garbage without default``.Value)
+
+    [<Test>]
+    let ``Garbage variable with default is not set`` () =
+        Assert.IsFalse(``Garbage with default``.IsSet)
+
+    [<Test>]
+    let ``Garbage variable with default is default`` () =
+        Assert.AreEqual("some default value", ``Garbage with default``.Value)
